53925f9aa691ffb278f2a5ea649e8d9119948089
Kanishk Gandhi <kanishk050296@gmail.com>
2024-04-24 18:04:11 -0700

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -4,4 +4,4 @@ conda activate sos
 
 cd src
 
-accelerate launch train.py --config ../configs/sft-mix-4-cd5e5.conf
+accelerate launch --config_file ../configs/accelerate.yaml train.py --config ../configs/sft-mix-4-cd5e5.conf
