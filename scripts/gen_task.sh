55fbbd9095d6c57d7538b6aff650bbd64b7d2519
Kanishk Gandhi <kanishk050296@gmail.com>
2024-04-25 17:12:40 -0700

    Update gen_task.sh

--- scripts/gen_task.sh
+++ scripts/gen_task.sh
@@ -4,4 +4,4 @@ conda activate sos
 
 cd src
 
-python countdown_generate.py --seed 4 --data_dir data/b4_3_random/ --min_range 4 --start_range 4 --num_samples 220000 --grow --offset 2
+python countdown_generate.py --seed 4 --data_dir data/b4_3_random/ --min_range 4 --start_range 4 --num_samples 500000
